d475a2c54a35e24e1c5288bd7d68161dc5fa57c3
David Jones <dcejjones@gmail.com>
2018-12-12 06:38:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay() {
+  var musicians = [];
+  var instruments = [];
+  var musicTime = [];
+  for (var i = 0; i < musicians.length; ++i) {
+    musicTime = [(musicians+instruments)];
+  }
+}
+
+function johnLennonFacts() {
+  var facts = [];
+  while (i <= facts.length) {
+    return 
+  }
+}
+
+function iLoveTheBeatles(y) {
+  var emptyArr = [];
+  do {
+    emptyArr.push("I love the Beatles!");
+  }
+  while (y < 15) {
+    y++;
+  }
+}
